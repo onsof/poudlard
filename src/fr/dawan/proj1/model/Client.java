a52d9bb4dfadbfcdcf8aa4526f42fa7266557c39
astrid <onsof@hotmail.fr>
2018-09-04 16:04:30 +0200

    ajout de a et b variable

--- src/fr/dawan/proj1/model/Client.java
+++ src/fr/dawan/proj1/model/Client.java
@@ -2,9 +2,12 @@ package fr.dawan.proj1.model;
 
 public class Client {
 
+	private int a = 0;
+	private String b = " harry potter";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
